927ebd6948326a211434d17323705c02a7e05886
Justin <justinknueppel@gmail.com>
2020-05-16 21:50:41 -0400

    Finish 3.8

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+morgan.token("body", (request, response) => {
+  return request.method.toUpperCase() === "POST"
+    ? JSON.stringify(request.body)
+    : "";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
