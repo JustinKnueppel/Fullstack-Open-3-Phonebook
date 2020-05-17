dcf6df1fcf0e9e1768d0aa6efb6083a327ba7f42
Justin <justinknueppel@gmail.com>
2020-05-16 21:28:36 -0400

    Add basic CRUD with error handling

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+app.use(express.json());
+
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -25,15 +27,68 @@ const persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.send(persons);
-});
+const checkValidNewPerson = (newPerson) => {
+  if (!newPerson.name) {
+    return "Person must have a name";
+  }
+  if (!newPerson.number) {
+    return "Person must have a number";
+  }
+  if (persons.map((person) => person.name).includes(newPerson.name)) {
+    return `${newPerson.name} is already in the phonebook`;
+  }
+  return "";
+};
 
 app.get("/info", (request, response) => {
   const date = Date();
   const numPersons = persons.length;
-  response.send(`<p>Phonebook as info for ${numPersons} people</p><p>${date}</p>`)
-})
+  response.send(
+    `<p>Phonebook as info for ${numPersons} people</p><p>${date}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  if (!persons.map((person) => person.id).includes(id)) {
+    return response.status(404).end();
+  }
+
+  const person = persons.find((person) => person.id === id);
+  response.json(person);
+});
+
+app.post("/api/persons", (request, response) => {
+  const newPerson = request.body;
+
+  const error = checkValidNewPerson(newPerson);
+  if (error) {
+    return response.status(400).json({ error }).end();
+  }
+
+  const id = Math.floor(Math.random() * Math.pow(2, 12));
+
+  persons = persons.concat({ ...newPerson, id });
+
+  response.status(201).send({ id });
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  if (!persons.map((person) => person.id).includes(id)) {
+    return response.status(204).end();
+  }
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT);
