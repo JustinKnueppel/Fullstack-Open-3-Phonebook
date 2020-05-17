c845b53b8df01514f84f1b742d9d8447211eec3e
Justin <justinknueppel@gmail.com>
2020-05-17 17:20:08 -0400

    add frontend build files

---
+++ build/precache-manifest.e5d222564aeb73ff75fa520f91efca51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf0616db3bf6c4193289d629d8119a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2148830170829aaeeee1",
+    "url": "/static/css/main.bcd433fd.chunk.css"
+  },
+  {
+    "revision": "d5b9e9c7a493abe5ae2d",
+    "url": "/static/js/2.d3c29f6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3c29f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2148830170829aaeeee1",
+    "url": "/static/js/main.5221c8be.chunk.js"
+  },
+  {
+    "revision": "3e6513b2f121a3a78a08",
+    "url": "/static/js/runtime-main.ae380888.js"
+  }
+]);
